d8dd3aae942eaca8a2d9684007b99e7397627b3d
Asel <asel.alimbaeva.kk@gmail.com>
2025-04-27 19:11:54 +0300

    feat: add code for env fn

--- node-nodejs-basics-main/src/cli/env.js
+++ node-nodejs-basics-main/src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+  const variables = Object.entries(process.env)
+    .filter(([key]) => key.startsWith('RSS_'))
+    .map(([key, value]) => `${key}=${value}`)
+    .join('; ');
+
+  console.log(variables);
 };
 
 parseEnv();
