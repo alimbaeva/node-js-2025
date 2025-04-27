1e6e340221782ad651f52f8e8cef499cd3396311
Asel <asel.alimbaeva.kk@gmail.com>
2025-04-27 20:57:58 +0300

    feat: add code for stream read fn

--- node-nodejs-basics-main/src/streams/read.js
+++ node-nodejs-basics-main/src/streams/read.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const read = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+  const fileStream = fs.createReadStream(filePath, 'utf-8');
+  
+  fileStream.on('data', (chunk) => {
+      process.stdout.write(chunk);
+  });
+
+  fileStream.on('end', () => {
+      console.log('\nЧтение файла завершено.');
+  });
+
+  fileStream.on('error', (err) => {
+      console.error('Ошибка при чтении файла:', err);
+  });
 };
 
 await read();
