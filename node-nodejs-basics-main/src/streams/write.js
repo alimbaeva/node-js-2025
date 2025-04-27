c76dab0cdd17d4d80eb5b7d7bcc885db8cea3ba1
Asel <asel.alimbaeva.kk@gmail.com>
2025-04-27 21:02:12 +0300

    feat: add code for stream write fn

--- node-nodejs-basics-main/src/streams/write.js
+++ node-nodejs-basics-main/src/streams/write.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+  const writeStream = fs.createWriteStream(filePath, { encoding: 'utf-8' });
+  process.stdin.pipe(writeStream); 
+ 
+  writeStream.on('error', (err) => {
+  console.error('Ошибка при записи файла:', err.message);
+  });
 };
 
 await write();
