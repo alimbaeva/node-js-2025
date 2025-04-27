b35f185fbb92b523b081e2976c13e47b87d3ad91
Asel <asel.alimbaeva.kk@gmail.com>
2025-04-27 21:06:10 +0300

    feat: add code for stream transform fn

--- node-nodejs-basics-main/src/streams/transform.js
+++ node-nodejs-basics-main/src/streams/transform.js
@@ -1,5 +1,18 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+  const transform = new Transform({
+    transform(chunk, encoding, callback) {
+      const transChunk = chunk
+        .toString()
+        .split('')
+        .map((char) =>
+          char === char.toLowerCase() ? char.toUpperCase() : char.toLowerCase()
+        )
+        .join('');
+      callback(null, transChunk);
+    },  });  
+  process.stdin.pipe(transform).pipe(process.stdout);
 };
 
 await transform();
