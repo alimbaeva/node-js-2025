5e34048b9e5e600ad728b68d910d457cbc536afc
Asel <asel.alimbaeva.kk@gmail.com>
2025-04-27 17:21:06 +0300

    feat: add code for copy fn

--- node-nodejs-basics-main/src/fs/copy.js
+++ node-nodejs-basics-main/src/fs/copy.js
@@ -1,5 +1,27 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+  const sourceDir = path.join(__dirname, 'files');
+  const destDir = path.join(__dirname, 'files_copy');
+  try {
+    await fs.access(sourceDir);
+    try {
+        await fs.access(destDir);
+        throw new Error('FS operation failed');
+    } catch (err) {
+        if (err.code !== 'ENOENT') {
+          throw err;
+        }
+    }
+    await fs.cp(sourceDir, destDir, { recursive: true });
+  } catch(err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await copy();
