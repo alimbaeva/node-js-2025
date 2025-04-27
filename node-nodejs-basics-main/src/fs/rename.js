4e30b7f842aaf3060b95bcd0e596312177f24d14
Asel <asel.alimbaeva.kk@gmail.com>
2025-04-27 17:58:42 +0300

    feat: add code for rename fn

--- node-nodejs-basics-main/src/fs/rename.js
+++ node-nodejs-basics-main/src/fs/rename.js
@@ -1,5 +1,29 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const rename = async () => {
-    // Write your code here 
+  const oldPath = path.join(__dirname, 'files', 'wrongFilename.txt');
+  const newPath = path.join(__dirname, 'files', 'properFilename.md');
+  try {
+    await fs.access(oldPath);
+
+    try {
+        await fs.access(newPath);
+        throw new Error('FS operation failed');
+    } catch(err) {
+        if (err.code !== 'ENOENT') {
+            throw err;
+        }
+    }
+
+    await fs.rename(oldPath, newPath);
+  } catch(err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await rename();
