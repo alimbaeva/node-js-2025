4101fdd6b7a5e1ee4044f6a26828dd2b14ecd945
Asel <asel.alimbaeva.kk@gmail.com>
2025-04-28 11:31:45 +0300

    feat: add code for wt fn

--- node-nodejs-basics-main/src/wt/main.js
+++ node-nodejs-basics-main/src/wt/main.js
@@ -1,5 +1,34 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const performCalculations = async () => {
-    // Write your code here
+  const numCPUs = os.cpus().length;
+  const workers = [];
+
+  for (let i = 0; i < numCPUs; i++) {
+      const worker = new Worker(new URL('./worker.js', import.meta.url), { type: 'module' });
+      workers.push(worker);
+  }
+
+  const promises = workers.map((worker, index) => {
+      return new Promise((resolve) => {
+          worker.on('message', (result) => {
+              resolve({ status: 'resolved', data: result });
+          });
+          worker.on('error', () => {
+              resolve({ status: 'error', data: null });
+          });
+          worker.postMessage(10 + index);
+      });
+  });
+
+  const finalResults = await Promise.all(promises);
+  console.log(finalResults);
 };
 
 await performCalculations();
