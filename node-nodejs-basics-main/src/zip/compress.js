8fd20207b77a4eab6ef8633024381c072fc83d30
Asel <asel.alimbaeva.kk@gmail.com>
2025-04-28 11:08:31 +0300

    feat: add code for compress fn

--- node-nodejs-basics-main/src/zip/compress.js
+++ node-nodejs-basics-main/src/zip/compress.js
@@ -1,5 +1,14 @@
+import { createGzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { join } from 'path';
+
 const compress = async () => {
-    // Write your code here 
+  const fileToCompressPath = join('src', 'zip', 'files', 'fileToCompress.txt');
+  const archivePath = join('src', 'zip', 'files', 'archive.gz');
+  const readable = createReadStream(fileToCompressPath);
+  const writeable = createWriteStream(archivePath);
+  const gzip = createGzip();
+  readable.pipe(gzip).pipe(writeable);
 };
 
 await compress();
