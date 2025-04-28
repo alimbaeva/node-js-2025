cf515beceb22e0f5c232d274fd1972a052804e3c
Asel <asel.alimbaeva.kk@gmail.com>
2025-04-28 11:32:16 +0300

    feat: add code for wt fn

--- node-nodejs-basics-main/src/wt/main.js
+++ node-nodejs-basics-main/src/wt/main.js
@@ -1,10 +1,5 @@
 import { Worker } from 'worker_threads';
 import os from 'os';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const performCalculations = async () => {
   const numCPUs = os.cpus().length;
