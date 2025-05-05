2da667ec496d28332938228e1f829796567a9098
Asel <asel.alimbaeva.kk@gmail.com>
2025-05-05 21:36:23 +0300

    feat: add

--- file-manager/src/cli/cli.js
+++ file-manager/src/cli/cli.js
@@ -2,6 +2,7 @@ import readline from 'readline';
 import { chdir } from 'process';
 import { getUsername, getHomeDirectory } from '../init/init.js';
 import { goUp } from '../commands/goUp.js';
+import { add } from '../commands/add.js';
 import { cat } from '../commands/readCat.js';
 import { changeDirectory } from '../commands/changeDirectory.js';
 import { listDirectory } from '../commands/listDirectory.js';
@@ -55,6 +56,14 @@ rl.on('line', async (line) => {
             console.log('Invalid input');
         } else {
             cat(args[0]);
+        }
+        break;
+
+      case 'add':
+        if (args.length === 0) {
+          console.log('Invalid input');
+        } else {
+          add(args[0]);
         }
         break;
 
