ed14a46f86a0999c50b757a116d97954084e5812
Asel <asel.alimbaeva.kk@gmail.com>
2025-05-09 11:37:11 +0300

    feat: resolveValue should resolve provided value & throwError

--- basic-testing-main/src/03-error-handling-async/index.test.ts
+++ basic-testing-main/src/03-error-handling-async/index.test.ts
@@ -1,19 +1,28 @@
-// Uncomment the code below and write your tests
+import { throwError, resolveValue } from './index';
 // import { throwError, throwCustomError, resolveValue, MyAwesomeError, rejectCustomError } from './index';
 
 describe('resolveValue', () => {
   test('should resolve provided value', async () => {
-    // Write your test here
+    const valueNumber = 42;
+    const valueString = 42;
+    const valueBoolean = 42;
+    const result1 = await resolveValue(valueNumber);
+    const result2 = await resolveValue(valueString);
+    const result3 = await resolveValue(valueBoolean);
+    expect(result1).toBe(valueNumber);
+    expect(result2).toBe(valueString);
+    expect(result3).toBe(valueBoolean);
   });
 });
 
 describe('throwError', () => {
   test('should throw error with provided message', () => {
-    // Write your test here
+    const errorMessage = 'Something went wrong';
+    expect(() => throwError(errorMessage)).toThrow(errorMessage);
   });
 
   test('should throw error with default message if message is not provided', () => {
-    // Write your test here
+    expect(() => throwError()).toThrow('Oops!');
   });
 });
 
