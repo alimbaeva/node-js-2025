7d61d1ac3db51f1f96ed014b1996536013ce62d4
Asel <asel.alimbaeva.kk@gmail.com>
2025-05-09 10:05:43 +0300

    refactor: tests

--- basic-testing-main/src/01-simple-tests/index.test.ts
+++ basic-testing-main/src/01-simple-tests/index.test.ts
@@ -1,25 +1,16 @@
 import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
-  test.each([
-    { input: { a: 2, b: 3, action: Action.Add }, expected: 5 },
-    { input: { a: -2, b: 3, action: Action.Add }, expected: 1 },
-    { input: { a: 0, b: 0, action: Action.Add }, expected: 0 },
-    { input: { a: 1.5, b: 2.5, action: Action.Add }, expected: 4 },
-  ])('should add two numbers', ({ input, expected }) => {
+  test('should add two numbers', () => {
+    const input = { a: 2, b: 3, action: Action.Add };
     const result = simpleCalculator(input);
-    expect(result).toBe(expected);
+    expect(result).toBe(5);
   });
 
-  test.each([
-    { input: { a: 5, b: 2, action: Action.Subtract }, expected: 3 },
-    { input: { a: 15, b: 2, action: Action.Subtract }, expected: 13 },
-    { input: { a: 10, b: 9, action: Action.Subtract }, expected: 1 },
-    { input: { a: 10, b: -9, action: Action.Subtract }, expected: 19 },
-    { input: { a: -10, b: -9, action: Action.Subtract }, expected: -1 },
-  ])('should subtract two numbers', ({ input, expected }) => {
+  test('should subtract two numbers', () => {
+    const input = { a: 5, b: 2, action: Action.Subtract };
     const result = simpleCalculator(input);
-    expect(result).toBe(expected);
+    expect(result).toBe(3);
   });
 
   test('should multiply two numbers', () => {
