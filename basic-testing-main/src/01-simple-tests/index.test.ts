344304a9ac7e457347304232de9a470540f1ac0c
Asel <asel.alimbaeva.kk@gmail.com>
2025-05-09 09:50:47 +0300

    feat: add test for should add two numbers & should subtract two numbers

--- basic-testing-main/src/01-simple-tests/index.test.ts
+++ basic-testing-main/src/01-simple-tests/index.test.ts
@@ -1,13 +1,16 @@
-// Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const input = { a: 2, b: 3, action: Action.Add };
+    const result = simpleCalculator(input);
+    expect(result).toBe(5);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const input = { a: 5, b: 2, action: Action.Subtract };
+    const result = simpleCalculator(input);
+    expect(result).toBe(3);
   });
 
   test('should multiply two numbers', () => {
