39c4e89fdd9d4a2452ebc5e90b287ac1dcc4bcce
Asel <asel.alimbaeva.kk@gmail.com>
2025-05-17 20:46:50 +0300

    feat: add reg

---
+++ websocket-battleship-server/src/ws_server/messageHandler.js
@@ -0,0 +1,13 @@
+import { handleRegistration } from '../controllers/player.js';
+
+export function handleMessage(ws, message) {
+  const { type, data, id } = message;
+
+  switch (type) {
+    case 'reg':
+      handleRegistration(ws, data, id);
+      break;
+    default:
+      ws.send(JSON.stringify({ type: 'error', data: JSON.stringify({ message: 'Неизвестный тип сообщения' }), id }));
+  }
+}
